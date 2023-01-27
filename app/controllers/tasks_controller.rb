450cf73675db71eaee021558c8688fde911f5ebb
Weston Sandfort <sandfortw@gmail.com>
2023-01-27 11:27:51 -0700

    finish the CRUD functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,14 +7,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    require 'pry'; binding.pry
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
       })
-    require 'pry'; binding.pry
     task.save
-    require 'pry'; binding.pry
+    redirect_to '/tasks'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
     redirect_to '/tasks'
   end
 end
