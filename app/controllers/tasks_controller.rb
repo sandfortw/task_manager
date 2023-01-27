c565ef4cb4b5b873d546c34aef2232fa3abcdb5a
Weston Sandfort <sandfortw@gmail.com>
2023-01-27 10:52:49 -0700

    Can now add tasks to index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,20 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = Task.all
   end
 
   def new
   end
 
   def create
+    require 'pry'; binding.pry
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
       })
-
+    require 'pry'; binding.pry
     task.save
-
+    require 'pry'; binding.pry
     redirect_to '/tasks'
   end
 end
